4b07e1cded6872945aecb037abb39d726d67b381
Mike Bannister <notimpossiblemike@gmail.com>
2015-02-07 12:52:18 -0800

    Seperate crop marks component and css

--- components/player_adapter/iframeless/index.jsx
+++ components/player_adapter/iframeless/index.jsx
@@ -8,49 +8,9 @@ var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 var PlayerAdapter = require('../index');
 
 var IframelessPlayerAPI = require('./player_api');
+var CropMarks = require('../../crop_marks');
 
 require('./index.styl');
-
-var CropMarks = React.createClass({
-  render: function() {
-    return (
-      <table className="crop-marks" border="0"
-            cellPadding="0" cellSpacing="0" width="100%">
-        <tbody>
-          <tr key="head" className="head">
-            <td className="corner top left">
-              <div className="crop-mark-shim"></div>
-            </td>
-            <td>
-            </td>
-            <td className="corner top right">
-              <div className="crop-mark-shim"></div>
-            </td>
-          </tr>
-          <tr key="body" className="body">
-            <td key="left">
-            </td>
-            <td key="middle" className="middle">
-              {this.props.children}
-            </td>
-            <td key="right">
-            </td>
-          </tr>
-          <tr key="foot" className="foot">
-            <td className="corner bottom left">
-              <div className="crop-mark-shim"></div>
-            </td>
-            <td>
-            </td>
-            <td className="corner bottom right">
-              <div className="crop-mark-shim"></div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    );
-  }
-});
 
 var DebugTools = React.createClass({
   getInitialState: function() {
