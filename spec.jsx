cd528f5e4df50ef4be071e1366080d9e0a70852d
Mike Bannister <notimpossiblemike@gmail.com>
2015-02-15 18:08:27 -0800

    Explicit about what's being required

--- spec.jsx
+++ spec.jsx
@@ -5,7 +5,7 @@ var TestUtils = React.addons.TestUtils;
 var renderComponent = TestUtils.renderIntoDocument;
 var findComponent = TestUtils.findRenderedDOMComponentWithClass;
 
-var PlayerAdapter = require('.');
+var PlayerAdapter = require('./index');
 var VersalPlayerAPI = require('versal-gadget-api/src/player-api');
 
 var testManifest = {
